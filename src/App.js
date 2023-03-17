c18fc552089c3dc1cba8974b021db29e7cd36cc7
eleanora <eleanora.ms@gmail.com>
2023-03-17 17:12:18 -0300

    requisito 2 concluído!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 
 function App() {
   return (
     <>
-    <div className="App" />
-    <Switch>
-      <Route exact path="/" component={ Home }></Route>
-    </Switch>
+      <div className="App" />
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
-import { getProductsFromCategoryAndQuery } from '../services/api'
+// import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
   state = {
     listagemProdutos: [],
-  }
+  };
 
-  getProductList = async (category, query) => {
-    const productList = await getProductsFromCategoryAndQuery(category, query);
-    this.setState({
-      listagemProdutos: productList
-    })
-  }
+  // getProductList = async (category, query) => {
+  //   const productList = await getProductsFromCategoryAndQuery(category, query);
+  //   this.setState({
+  //     listagemProdutos: productList,
+  //   });
+  // };
 
- render() {
-  const { listagemProdutos } = this.state;
+  render() {
+    const { listagemProdutos } = this.state;
     return (
       <div>
         <label>
-        <input type="text" />
+          <input type="text" />
         </label>
         {
-          listagemProdutos.length === 0 ? (<p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>) : <ul>
-          <li></li>
-        </ul>   
+          listagemProdutos.length === 0
+            ? (
+              <p data-testid="home-initial-message">
+                Digite algum termo de pesquisa ou escolha uma categoria.
+              </p>
+            )
+            : (
+              <ul>
+                <li />
+              </ul>
+            )
         }
       </div>
 
     );
- }
-};
+  }
+}
 
 export default Home;
