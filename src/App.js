321535483e814b9ea0dc024c6051050c0522438e
eleanora <eleanora.ms@gmail.com>
2023-03-17 16:59:51 -0300

    requisito 2 concluído!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
+    <>
     <div className="App" />
+    <Switch>
+      <Route exact path="/" component={ Home }></Route>
+    </Switch>
+    </>
   );
 }
 
