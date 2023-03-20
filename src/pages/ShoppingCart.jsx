a451b906ab922b1ef8db0aac69099a31ec5e2e1f
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-03-20 20:32:35 -0300

    resolve parcialmente req08

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
+  state = {
+    cartProduct: JSON.parse(localStorage.getItem('cart-products')),
+  };
+
   render() {
+    const { cartProduct } = this.state;
+    console.log(cartProduct);
     return (
       <div>
-        <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+        {
+          cartProduct.length === 0
+            ? (
+              <p data-testid="home-initial-message">
+                Seu carrinho está vazio
+              </p>
+            )
+            : (
+              <section>
+                {
+                  cartProduct.map((produto) => (
+                    <div key={ produto.id }>
+                      <p
+                        data-testid="shopping-cart-product-name"
+                      >
+                        {produto.title}
+                      </p>
+                      <p>
+                        $
+                        {produto.price}
+                      </p>
+                    </div>
+                  ))
+                }
+              </section>
+            )
+        }
       </div>
     );
   }
